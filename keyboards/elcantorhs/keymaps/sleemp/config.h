ef399b707c40c0b13dd92f0f944031cc843f452e
Iosif Pravets <i@pravets.ru>
2024-11-27 00:57:10 +0300

    Added vial keymap for elcantorhs

---
+++ keyboards/elcantorhs/keymaps/sleemp/config.h
@@ -0,0 +1,11 @@
+
+/* keyboard uid */
+#define VIAL_KEYBOARD_UID {0xD3, 0x47, 0x4E, 0x68, 0xDE, 0x96, 0x2E, 0x0E}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+
+
